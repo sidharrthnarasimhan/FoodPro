c520fe1608a242c822e390b6a39fb299a36176ad
Sidharrth Narasimhan <nsidharrth@gmail.com>
2025-01-27 18:06:58 -0800

    Initial Commit

---
+++ FoodPro/FoodProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodProApp.swift
+//  FoodPro
+//
+//  Created by sidharrth narasimhan on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
